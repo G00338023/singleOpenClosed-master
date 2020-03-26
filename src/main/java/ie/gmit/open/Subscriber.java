4d7620a94c4972e48d06c934641c1ea88dc03d6f
Cillin <G00338023@gmit.ie>
2020-03-26 22:40:43 +0000

    Initial commit

---
+++ src/main/java/ie/gmit/open/Subscriber.java
@@ -0,0 +1,35 @@
+package ie.gmit.open;
+
+public abstract class Subscriber {
+
+    public Long subscriberId;
+
+    private String address;
+
+    private Long phoneNumber;
+
+    public Long getSubscriberId() {
+        return subscriberId;
+    }
+
+    public void setSubscriberId(Long subscriberId) {
+        this.subscriberId = subscriberId;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(Long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    public abstract double calculateBill();
+}
